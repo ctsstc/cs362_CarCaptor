008e062d04990e36b2a7735219bb2bae12f0afba
Cody Swartz <ctsstc@gmail.com>
2019-05-29 11:54:14 -0700

    Remove setup from garage

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,14 +1,6 @@
 class Garage < ApplicationRecord
-    belongs_to :user
-  has_many :car
-  attr_accessor :cars
-
-  def setup(params = {})
-    self.user = params.fetch(:user, User.new)
-    self.capacity = params.fetch(:capacity, 1)
-    @cars = []
-    self.car_count = 0
-  end
+  belongs_to :user
+  has_many :cars
 
   def total_coolness
      cars.sum(:coolness_value)
