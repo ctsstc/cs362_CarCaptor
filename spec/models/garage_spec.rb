cb92127879fb440f27ff4c2fbc641b455885197e
Cody Swartz <ctsstc@gmail.com>
2019-05-29 12:17:22 -0700

    Update garage specs to use factories and new relations

--- spec/models/garage_spec.rb
+++ spec/models/garage_spec.rb
@@ -1,27 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Garage, type: :model do
-    garage = Garage.new
+  let!(:user) { create(:user) }
+  let(:garage) { create(:garage, user: user) }
 
-    it "expects a new garage to have no cars" do
-      expect(garage.cars.length).to eq(0) 
+  describe 'empty garage' do
+    it 'expects a new garage to have no cars' do
+      expect(garage.cars.length).to eq(0)
     end
 
-    it "expects a garage with no cars to be lame" do
+    it 'expects a garage with no cars to be lame' do
       expect(garage).to be_lame
     end
+  end
 
-    describe "adding cars" do
-      it "keeps track of the number of cars added" do
-        garage.add(Car.new)
-        expect(garage.cars.length).to eq(1)
-            garage.add
-            expect(garage.cars.length).to eq(2)
-            expect(garage.count_cars).to eq(2)
-      end
+  describe 'garage with a car' do
+    let(:car) { create(:car) }
 
-      it "expects a garage with at least one car with a positive coolness value to not be lame" do
-        expect(garage).to_not be_lame
-      end
+    before do
+      garage.add(car)
     end
+
+    it 'has 1 car' do
+      expect(garage.cars.length).to eq(1)
+    end
+
+    it 'is not lame' do
+      expect(garage).to_not be_lame
+    end
+
+    it 'increases car amount by 1' do
+      expect { garage.add }.to change { garage.cars.length }.by(1)
+    end
+  end
 end
