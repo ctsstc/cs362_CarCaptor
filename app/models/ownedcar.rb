9c2145e9f46e00038f9e59b1ad93fb7d65d8850b
Cody Swartz <ctsstc@gmail.com>
2019-06-01 00:28:15 -0700

    Clean up car & owned car model

--- app/models/ownedcar.rb
+++ app/models/ownedcar.rb
@@ -1,17 +1,6 @@
 class Ownedcar < ApplicationRecord
   belongs_to :user
 
-  def setup(params = {})
-    self.year = params.fetch(:year, 1)
-    self.make = params.fetch(:make, 'automobile')
-    self.model = params.fetch(:model, 'car')
-    self.coolness_value = params.fetch(:coolness_value, 1)
-    self.horsepower = params.fetch(:horsepower, 1)
-    self.torque = params.fetch(:torque, 1)
-    self.weight = params.fetch(:weight, 1)
-    self.user = params.fetch(:user, User.new)
-  end
-
   def calculate_power_to_weight_ratio
     horsepower.fdiv(weight)
   end
