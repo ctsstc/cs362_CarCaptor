ae10eca47620d49375485c2543bdb928f6c5d3a3
Cody Swartz <ctsstc@gmail.com>
2019-06-01 00:28:26 -0700

    Rubocop autofix round 1

--- spec/factories/ownedcar.rb
+++ spec/factories/ownedcar.rb
@@ -1,25 +1,27 @@
-FactoryBot.define do
-	factory :ownedcar do
-		trait :civic do
-			year {2001}
-			make {"Honda"}
-			model {"Civic"}
-			coolness_value {1}
-			horsepower {190}
-			torque {220}
-			weight {3690}
-			user {User.new}
-		end
+# frozen_string_literal: true
 
-		trait :ae86 do
-			year {1986}
-			make {"Toyota"}
-			model {"AE86"}
-			coolness_value {100}
-			horsepower {190}
-			torque {115}
-			weight {2200}
-			user {User.new}
-		end
-	end
+FactoryBot.define do
+  factory :ownedcar do
+    trait :civic do
+      year { 2001 }
+      make { 'Honda' }
+      model { 'Civic' }
+      coolness_value { 1 }
+      horsepower { 190 }
+      torque { 220 }
+      weight { 3690 }
+      user { User.new }
+    end
+
+    trait :ae86 do
+      year { 1986 }
+      make { 'Toyota' }
+      model { 'AE86' }
+      coolness_value { 100 }
+      horsepower { 190 }
+      torque { 115 }
+      weight { 2200 }
+      user { User.new }
+    end
+  end
 end
