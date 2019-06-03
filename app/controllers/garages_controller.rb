9e10f36ba17691e9f4140beb68c5c3f88d610498
abell95 <belland@oregonstate.edu>
2019-06-03 10:38:54 -0700

    Add logic to garages controller, create routes for garage and garage/new

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,4 +1,20 @@
-# frozen_string_literal: true
-
 class GaragesController < ApplicationController
+  def new
+    @garage = Garage.new
+  end
+  
+  def index
+    @garages = Garage.all
+  end
+
+  def create
+    garage = Garage.create(
+      capacity: params[:garage][:capacity]
+    )
+    if garage.save
+      redirect_to garages_path
+    else
+      render :new
+    end
+  end
 end
