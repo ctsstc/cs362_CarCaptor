9ab2edaea7386e9cb036776add34429ffe18f158
Cody Swartz <ctsstc@gmail.com>
2019-06-09 16:45:03 -0700

    Add devise and dotenv gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/' => 'rootcontroller#index'
+  root to: 'rootcontroller#index'
   resources :ownedcars
   resources :garages
   resources :users
