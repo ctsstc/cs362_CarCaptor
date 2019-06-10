4881066fcc7f4511f05bab838f237edbd4889ae4
Cody Swartz <ctsstc@gmail.com>
2019-06-10 00:47:05 -0700

    Clean up add user system test to match new devise flow

--- spec/system/add_user_spec.rb
+++ spec/system/add_user_spec.rb
@@ -4,18 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'adding a user', type: :system do
   it 'allows a new user to be created' do
-    visit new_user_path
+    visit root_path
+    click_on('Register')
+    # visit new_user_registration_path
     fill_in 'Username', with: 'Dalton'
     fill_in 'Password', with: 'bigpass123'
-    fill_in 'Email', with: 'example@gmail.com'
-    click_on('Create User')
-    visit users_path
-    @user = User.find_by(username: 'Dalton')
-    expect(page).to have_selector("#user_#{@user.id} .username", text: 'Dalton')
-    expect(page).to have_selector("#user_#{@user.id} .password", text: 'bigpass123')
-    expect(page).to have_selector("#user_#{@user.id} .email", text: 'example@gmail.com')
-    visit user_path(@user)
-    expect(page).to have_content('Dalton')
-    expect(page).to have_content('example@gmail.com')
+    fill_in 'user_password_confirmation', with: 'bigpass123'
+    fill_in 'user_email', with: 'example@gmail.com'
+    click_on('Sign up')
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Hello Dalton')
   end
 end
