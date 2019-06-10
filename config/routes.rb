a38a14d13fc0f0cc5b61f9964451f006457ade38
Cody Swartz <ctsstc@gmail.com>
2019-06-09 23:10:42 -0700

    Remove old users routes resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root to: 'rootcontroller#index'
   resources :ownedcars
   resources :garages
-  resources :users
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
